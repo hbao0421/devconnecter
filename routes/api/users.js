af76c3b2c3f49134f0c0da617dd314f3183e7629
hbao0421 <hbao20000421@gmail.com>
2023-01-14 23:16:15 -0500

    validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,18 @@
 const express = require('../../node_modules/express');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
 
 
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    res.send('User route')
+router.post('/',
+    body('name','Name is required').not().isEmpty(),
+    body('username','email should be valid').isEmail(),
+    body('password','password should be more  than 5').isLength({min:6}),
+    (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    res.send('User route');
 });
 
 module.exports = router;
