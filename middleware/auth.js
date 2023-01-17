946c476528bd1cf7b857a66b3c7fedbe7fd1eafd
hbao0421 <hbao20000421@gmail.com>
2023-01-16 22:24:36 -0500

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,11 +6,11 @@ module.exports = function(req,res,next){
     const token  = req.header('x-auth-token');
     //Check if no token
     if(!token){
-        return res/status(401).json({msg:'No token auth denied'});
+        return res.status(401).json({msg:'No token auth denied'});
     }
     try{
         const decoded = jwt.verify(token,config.get('jwtSecret'));
-        req.user = decoded = user;
+        req.user = decoded.user;
         next();
     }catch(err){
         res.status(401).json({msg:'Token is not valid'});
