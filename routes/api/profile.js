6be05203d35cddf8224629719748560618ff846f
hbao0421 <hbao20000421@gmail.com>
2023-01-28 16:22:32 -0500

    pro

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -99,4 +99,21 @@ router.get('/',async(req,res)=>{
         res.status(500).send('Server Error');
     }
 })
+
+//get profile by user id
+router.get('/user/:user_id',async(req,res)=>{
+    try{
+        const profile = await Profile.findOne({user:req.params.user_id}).populate('user',['name','avatar']);
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user'});
+        }
+        res.json(profile);
+    }catch(err){
+        console.log(err.message);
+        if(err.kind =='ObjectId'){
+            return res.status(400).json({msg:'Profile not found'});
+        }
+        res.status(500).send('Server Error');
+    }
+})
 module.exports = router;
