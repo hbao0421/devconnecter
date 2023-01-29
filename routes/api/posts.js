0ed32d79d75c01859672112578c34b7786f352c1
hbao0421 <hbao20000421@gmail.com>
2023-01-29 17:02:42 -0500

    like and cancel

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,7 +5,6 @@ const auth = require('../../middleware/auth');
 const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-const { post } = require('request');
 
 
 router.post('/',[auth,[
@@ -76,6 +75,39 @@ router.delete('/:id',auth,async(req,res)=>{
         if(err.kind==='ObejctId'){
             return res.status(404).json({msg:'Post not found'});
         }
+        res.status(500).send('Server Error');
+    }
+});
+
+//post like
+router.put('/like/:id',auth,async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.likes.filter(like=>like.user.toString()===req.user.id).length>0){
+            return res.status(400).json({msg:'Post already liked'});
+        }
+        post.likes.unshift({user:req.user.id});
+        await post.save();
+        res.json(post);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//post unlike
+router.put('/unlike/:id',auth,async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.likes.filter(like=>like.user.toString()===req.user.id).length===0){
+            return res.status(400).json({msg:'Post not liked yet'});
+        }
+        const removeIndex = post.likes.map(like=>like.user.toString()).indexOf(req.params.id);
+        post.likes.splice(removeIndex,1);
+        await post.save();
+        res.json(post);
+    }catch(err){
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
