8f2d787b4b04e88d5c755686ec0bd67d9d58ef49
hbao0421 <hbao20000421@gmail.com>
2023-01-29 15:49:47 -0500

    add post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,7 +1,33 @@
 const express = require('../../node_modules/express');
 const router = express.Router();
+const {body, validationResult} = require('express-validator');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 
-router.get('/',(req,res)=>res.send('Posts route'));
+router.post('/',[auth,[
+    body('text','Text is required').not().isEmpty(),
+]],async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+        const post = await newPost.save();
+        res.json(post);
+    }catch(err){
+        console.error(err);
+        res.status().send('Server Error');
+    }
+});
 
 module.exports = router;
