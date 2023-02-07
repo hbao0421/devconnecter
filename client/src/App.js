9d03ac7b1ab95157ced324e0875caaf06db062f2
hbao0421 <hbao20000421@gmail.com>
2023-02-07 13:52:31 -0500

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import React,{Fragment} from 'react'
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
+import React,{Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
 import './App.css';
 
 const App=()=> (
-  <Fragment>
-    <Navbar/>
-    <Landing/>
-  </Fragment>
-  
+  <Router>
+    <Fragment>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+      </Routes>
+      <section className='container'>
+        <Routes>
+          <Route exact path='/register' element={<Register/>}></Route>
+          <Route exact path='/login' element={<Login/>}></Route>
+        </Routes>
+      </section>
+    </Fragment>
+  </Router>  
 );
 
 export default App;
