86bfb02ae8a346585c48ca974a4b3349cd11ebcd
hbao0421 <hbao20000421@gmail.com>
2023-02-16 13:58:20 -0500

    redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,29 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import {BrowserRouter as Router, Route, Routes, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App=()=> (
-  <Router>
-    <Fragment>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Landing/>}/>
-      </Routes>
-      <section className='container'>
+  <Provider store={store}>
+      <Router>
+      <Fragment>
+        <Navbar/>
         <Routes>
-          <Route exact path='/register' element={<Register/>}></Route>
-          <Route exact path='/login' element={<Login/>}></Route>
+          <Route path='/' element={<Landing/>}/>
         </Routes>
-      </section>
-    </Fragment>
-  </Router>  
+        <section className='container'>
+          <Routes>
+            <Route exact path='/register' element={<Register/>}></Route>
+            <Route exact path='/login' element={<Login/>}></Route>
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>  
+  </Provider>
+
 );
 
 export default App;
